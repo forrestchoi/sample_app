02e7ed2b58ba92b138ced7a3115886547e1fd37c
forrest.choi <forrest.choi@samsung.com>
2011-09-19 17:12:40 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'dd40d8169ba44658afe9feb8246995e3f0a1d882f2c1d989942ce3db5a74f2b6414771103e4ed520b8ee54b052d78934c476c241ce856accc32fd374dd5b3006'
